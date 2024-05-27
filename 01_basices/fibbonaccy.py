234b42e2673c93955208ef191821fead4d0cb408
Prabha <prabhacodingworld@gmail.com>
2024-05-28 02:42:40 +0530

    fibo searise

---
+++ 01_basices/fibbonaccy.py
@@ -0,0 +1,13 @@
+#WAP a program in python to print fibonacci sequence up to n terms
+n=int(input("How many terms you want in fibonacci sequence? "))
+n1=0 #First term
+n2=1 #Second term
+print("Fibonacci Sequence : ")
+print(n1,n2,end=" ")
+i=1
+while(i<=n-2):
+    n3=n1+n2
+    print(n3,end=" ")
+    n1=n2
+    n2=n3
+    i=i+1
